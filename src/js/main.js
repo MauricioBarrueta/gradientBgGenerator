2adfb4786a19a80e848fad07e713c63147bd2d0d
Mauricio <mauba22@outlook.com>
2025-12-15 16:19:43 -0600

    Updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
-const color1 = document.querySelector(".color-1"), color2 = document.querySelector(".color-2"), color3 = document.querySelector('.color-3')
+const color1 = document.querySelector(".color-1"), color2 = document.querySelector(".color-2"), color3 = document.querySelector('.color-3'),
+    color3Checkbox = document.getElementById('enableColor3'), color3Values = document.getElementById('color-3-values'), color3Icon = document.querySelector('.toggle-icon')
 const clrRange1 = document.getElementById('clr1'), clrRangeValue1 = document.getElementById('range1Value'), 
     clrRange2 = document.getElementById('clr2'), clrRangeValue2 = document.getElementById('range2Value'),
     clrRange3 = document.getElementById('clr3'), clrRangeValue3 = document.getElementById('range3Value')
@@ -11,6 +12,15 @@ window.onload = () => {
     resetValues()
     generateLinearGradientCode()
 }
+
+/* Para habilitar o deshabilitar el 3er color */
+color3Checkbox.addEventListener('change', () => {
+  const enabled = color3Checkbox.checked
+
+  color3Values.classList.toggle('disabled', !enabled)
+  color3Icon.classList.toggle('fa-minus', enabled)
+  color3Icon.classList.toggle('fa-plus', !enabled)
+});
 
 /* Se obtiene la direccion de acuerdo al botón seleccionado y verifica cuál función se tiene que llamar */
 const getGradientDirection = (direction, clickedBtn) => {
@@ -26,16 +36,14 @@ const getGradientDirection = (direction, clickedBtn) => {
 
 /* Se genera el código y se aplican los estilos para la vista previa de 'linear-gradient' */
 const generateLinearGradientCode = () => {
-    let linearGradientCode = `linear-gradient(${gradientDefaultDirection}, ${color1.value.toUpperCase()} ${clrRange1.value}%, ${color2.value.toUpperCase()} ${clrRange2.value}%, ${color3.value.toUpperCase()} ${clrRange3.value}%)`
-
+    const linearGradientCode = `linear-gradient(${gradientDefaultDirection}, ${color1.value.toUpperCase()} ${clrRange1.value}%, ${color2.value.toUpperCase()} ${clrRange2.value}%${enableColor3.checked ? `, ${color3.value.toUpperCase()} ${clrRange3.value}%` : ''})`
     cssCodeText.value = `background: ${color1.value.toUpperCase()};\nbackground: ${linearGradientCode};`
     gradientPreview.style.backgroundImage = linearGradientCode
 }
 
 /* Se genera el código y se aplican los estilos para la vista previa de 'radial-gradient' */
 const generateRadialGradientCode = () => {    
-    let radialGradientCode = `radial-gradient(${gradientDefaultDirection}, ${color1.value.toUpperCase()} ${clrRange1.value}%, ${color2.value.toUpperCase()} ${clrRange2.value}%, ${color3.value.toUpperCase()} ${clrRange3.value}%)`
-
+    const radialGradientCode = `radial-gradient(${gradientDefaultDirection}, ${color1.value.toUpperCase()} ${clrRange1.value}%, ${color2.value.toUpperCase()} ${clrRange2.value}%${enableColor3.checked ? `, ${color3.value.toUpperCase()} ${clrRange3.value}%` : '' })`
     cssCodeText.value = `background: ${color1.value.toUpperCase()};\nbackground: ${radialGradientCode};`
     gradientPreview.style.backgroundImage = radialGradientCode
 }
@@ -54,7 +62,11 @@ resetButton.addEventListener('click', () => {
 })
 
 const resetValues = () => {
-    clrRange1.value = clrRangeValue1.value = 0, clrRange2.value = clrRangeValue2.value = 100, clrRange3.value = clrRangeValue3.value = 100  
+    color3Checkbox.checked = false
+    color3Values.classList.toggle('disabled')
+    clrRange1.value = clrRangeValue1.value = 25
+    clrRange2.value = clrRangeValue2.value = 65  
+    clrRange3.value = clrRangeValue3.value = 100
     color1.value = '#F9DC5C', color2.value = '#FFFFFF', color3.value = '#000000'
     getGradientDirection('to bottom', document.querySelector('.def'))
 }
